5d5a7adb1ddeb8b79ef1398066146ff98fd13044
yzlucas@github.com <lucas@Lucass-iMac.local>
2020-10-05 16:43:05 -0700

    feat: set up the first part of HTML page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AppService]
 })
 export class AppComponent {
   title = 'tic-tac-toe-angular';
+  roomNumber = <Number> 0;
+  playedText = <string>'';
+  gameGrid = <Array<Object>>[];
+  private playedGameGrid = <Array<Object>>[];
+
+
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,12 @@
-import { Injectable } from "@angular/core";
-//import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Subject } from 'rxjs';
+import { Observable } from 'rxjs';
+import * as io from 'socket.io-client';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch'
+
 
 @Injectable()
 export class AppService {
@@ -16,5 +22,91 @@ export class AppService {
 				resolve(data);
 			});
 		});
+    }
+    
+    connectSocket(){
+        this.socket = io(this.BASE_URL);
+
+    }
+
+    /* Method to receive rooms-available event.*/
+	getRoomsAvailable(): any {
+		const observable = new Observable(observer => {
+			this.socket.on('rooms-available', (data) => {
+				observer.next(
+					data
+				);
+			});
+			return () => {
+				this.socket.disconnect();
+			};
+		});
+		return observable;
+	}
+	/* Method to create new room, create-room event.*/
+	createNewRoom(): any {
+		this.socket.emit('create-room', { 'test': 9909 });
+		const observable = new Observable(observer => {
+			this.socket.on('new-room', (data) => {
+				observer.next(
+					data
+				);
+			});
+			return () => {
+				this.socket.disconnect();
+			};
+		});
+		return observable;
+	}
+	/* Method to join new room, create-room event.*/
+	joinNewRoom(roomNumber): any {
+		this.socket.emit('join-room', { 'roomNumber': roomNumber });
+	}
+	/* Method to receive start-game event.*/
+	startGame(): any {
+		const observable = new Observable(observer => {
+			this.socket.on('start-game', (data) => {
+				observer.next(
+					data
+				);
+			});
+			return () => {
+				this.socket.disconnect();
+			};
+		});
+		return observable;
+	}
+	/* Method to join new room, create-room event.*/
+	sendPlayerMove(params): any {
+		this.socket.emit('send-move', params);
+	}
+	/* Method to receive start-game event.*/
+	receivePlayerMove(): any {
+		const observable = new Observable(observer => {
+			this.socket.on('receive-move', (data) => {
+				observer.next(
+					data
+				);
+			});
+			return () => {
+				this.socket.disconnect();
+			};
+		});
+		return observable;
+	}
+	/* Event to check the if any player is leaving the game */
+	playerLeft(): any {
+		const observable = new Observable(observer => {
+			this.socket.on('room-disconnect', (data) => {
+				observer.next(
+					data
+				);
+			});
+			return () => {
+				this.socket.disconnect();
+			};
+		});
+		return observable;
 	}
 }
+
