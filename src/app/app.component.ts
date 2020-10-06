68d3b96e2387e96005afe162309e320eefa4c472
yzlucas@github.com <lucas@Lucass-iMac.local>
2020-10-05 22:56:21 -0700

    feat: set up grid & PlayerTurn symbol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,30 @@ import { AppService } from './app.service';
   styleUrls: ['./app.component.css'],
   providers: [AppService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'tic-tac-toe-angular';
   roomNumber = <Number> 0;
   playedText = <string>'';
   gameGrid = <Array<Object>>[];
-  private playedGameGrid = <Array<Object>>[];
+  playedGameGrid = <Array<Object>>[];
+  displayPlayerTurn = <Boolean> true;
 
+  constructor(
+    private appService: AppService,
+  ){
+    this.gameGrid = appService.gameGrid;
+  }
 
+  ngOnInit(){
+    
+  }
+
+  renderPlayedText(number) {
+		if (this.playedGameGrid[number] === undefined) {
+			return '';
+		}else {
+			this.playedText = this.playedGameGrid[number]['player'];
+			return "this.playedText;"
+		}
+	}
 }
